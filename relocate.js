8c04e0abf91f9f6fb67cebf8f9054eb7fdcf1ef3
ytbg0512 <58429331+ytbg0512@users.noreply.github.com>
2020-06-28 00:20:49 +0900

    close settings popup when relocate

--- relocate.js
+++ relocate.js
@@ -3,6 +3,7 @@ const relocateBtn = document.querySelector("#relocate-button");
 function handleRelocateClick() {
     localStorage.removeItem("coords");
     cityText.innerText = "";
+    closeSettings();
     loadCoords();
 }
 
