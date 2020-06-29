2532242dca2a1a537cd92ddca08fc5b4a138bcf5
ytbg0512 <58429331+ytbg0512@users.noreply.github.com>
2020-06-29 22:18:57 +0900

    changed some text

--- clickAlert.js
+++ clickAlert.js
@@ -3,11 +3,11 @@ let clickCount = 0;
 function runClickEvent() {
     if (clickCount == 5) {
         ui.style.backgroundImage = "url(https://s3.ap-northeast-2.amazonaws.com/img.kormedi.com/news/culture/it/__icsFiles/artimage/2016/11/15/c_km60701/shutterstock_470843366.jpg)";
-        alert('김치찌개 모드 활성화됨');
+        alert('김치찌개 모드가 활성화됨');
     } else if (clickCount > 9) {
         ui.style.backgroundImage = "none";
         clickCount = 0;
-        alert('김치찌개 모드 비활성화됨');
+        alert('김치찌개 모드가 비활성화됨');
     }
 }
 
